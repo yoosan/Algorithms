b5b03be6cceb7d0918a3dd8f6263114ad1e0a10f
cliviazhou <576948259@qq.com>
2015-01-16 09:25:35 +0800

    update

--- Matrix.cpp
+++ Matrix.cpp
@@ -1,178 +1,23 @@
-//
-//  main.cpp
-//  Matrix
-//
-//  Created by zhou on 1/1/15.
-//  Copyright (c) 2015 Dimen. All rights reserved.
-//
-
 #include <iostream>
 using namespace std;
 
-template <typename T>
-class Matrix
+bool reverse(char* s, int start, int end)
 {
-    //static const int Max_Size = 1000;
-    
-    int row;
-    int column;
-    
-    T **twoDimenArray;
-    
-public:
-    explicit Matrix (int row, int column) {           //Constructor of Matrix
-        this ->row = row;
-        this ->column = column;
-        
-        if (row <= 0 || column <= 0)
-        {
-            cout << "Row or Column invaild." << endl;
-            return;
-        }
-        
-        twoDimenArray = new T *[row];
-        for (int i = 0;i < row; i++)
-            twoDimenArray[i] = new T[column];
-    };
-    
-    explicit Matrix (const Matrix &ref){             //Deep Copy constructor of Matrix.
-        this ->row = ref.row;
-        this ->column = ref.column;
-        
-        this -> twoDimenArray = new T *[this->row];
-        for (int i = 0; i < this->row; i++)
-            this -> twoDimenArray[i] = new T[this->column];
-        
-        for (int i = 0; i < this->row; i++)
-            for(int j = 0; j < this->column; j++)
-                this -> twoDimenArray[i][j] = ref.twoDimenArray[i][j];
-    }
-    
-    virtual ~Matrix ()                               //Destructor of Matrix
+    char tmp = '0';
+    int mid = start + (end - start) /2;
+    while (start < end)
     {
-        for (int i = 0; i < row; i++)
-            delete [] twoDimenArray[i];
-        //cout << "Destructor Successfule!" <<endl;
+        tmp = s[start];
+        s[start++] = s[end];
+        s[end--] = tmp;
     }
-    
-    
-    bool createMartix()                               //Create a Matrix
-    {
-        if (row <= 0 || column <= 0)
-            return false;
-        cout << "Please input the martix by first row then column." <<endl;
-        for (int i = 0; i < row; i++)
-            for (int j = 0; j < column; j++)
-                cin >> twoDimenArray[i][j];
-        return true;
-    }
-    
-    bool disPlay()                                    //Show the matrix by row first.
-    {
-        if(row <= 0 || column <= 0)
-            return false;
-        for (int i = 0; i < row; i++)
-        {
-            for (int j = 0; j < column; j++)
-                cout << twoDimenArray[i][j] << " ";
-            cout << endl;
-        }
-        return true;
-    }
-    
-    Matrix& operator= (const Matrix<T> &ref)            //Override the operator =
-    {
-        this -> row = ref.row;
-        this -> column = ref.column;
-        
-        for (int i = 0; i < this->row; i++)
-            for (int j = 0; j < this->column; j++)
-                this -> twoDimenArray[i][j] = ref.twoDimenArrar[i][j];
-        return *this;
-    }
-    
-    Matrix& operator+ (const Matrix<T> &ref)            //Override the operator +
-    {
-        if (this->row != ref.row || this->column != ref.column)
-            cout << "Error: They are not the same row and column. "<< endl;
-        
-        //Matrix<T> temp(ref.row, ref.column);
-        
-        for (int i = 0; i < this->row; i++)
-            for (int j = 0; j< this->column; j++)
-                this -> twoDimenArray[i][j] += ref.twoDimenArray[i][j];
-        return *this;
-    }
-    
-    Matrix& operator- (const Matrix<T> &ref)           //Override the operator -
-    {
-        if (this->row != ref.row || this->column != ref.column)
-            cout << "Error: They are not the same row and column. "<< endl;
-        
-        for (int i = 0; i < row ; i++)
-            for (int j = 0; j < column; j++)
-                this -> twoDimenArray[i][j] += ref.twoDimenArray[i][j];
-        return *this;
-    }
-    
-    Matrix& operator- ()
-    {
-        for (int i = 0; i < row; i++)
-            for (int j = 0; j < column; j++)
-                this->twoDimenArray[i][j] *= -1;
-        return *this;
-    }
-    
-    
-    Matrix& operator* (const Matrix<T> &ref)           //Override the operator *
-    {
-        if (this -> column != ref.row)
-        {
-            cout << "Error: Two Matrics which the first's column not equal to the second's row cant be multipy";
-        }
-        
-        int column = this -> column;;
-        this -> column = ref.column;
-        for (int i = 0; i < this -> row; i++)
-        {
-            for(int j = 0; j < ref.column; j++)
-            {
-                T temp = 0;
-                for (int k = 0; k < column; k++)
-                {
-                    temp += this->twoDimenArray[i][k] * ref.twoDimenArray[k][j];
-                }
-                this -> twoDimenArray[i][j] = temp;
-            }
-        }
-        return *this;
-    }
-    
-};
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    
-    Matrix<int> test(2, 4);
-    
-    test.createMartix();
-    
-    test.disPlay();
-    
-    Matrix<int> mu(4, 2);
-    
-    mu.createMartix();
-    
-    mu.disPlay();
-    
-    (test * mu).disPlay();
+    return true;
+}
 
-    //Matrix<int> an(test);
-    //an.disPlay();
-    
-    //(test + an).disPlay();
-    
-    //(-test).disPlay();
-    
-    
-    return 0;
+
+int main()
+{
+    char s[] = "abcd";
+    reverse(s);
+    cout << s <<endl;
 }
--- README.md
+++ README.md
@@ -1,11 +1,9 @@
 Algorithms
 ==========
 
-DataStructure and Algorithms
+####DataStructure and Algorithms
 
-All the code which inclue CPP, PY or other langs in this repository are the basic abstract datastructure and algorithms
-Including LinkedList, binarytree, binary-search-tree, stack, queue,hash-table and Graph.All the algorithms are correct 
-which can be applied in these datastructures.
+All the code which inclue **CPP**, **PY** or other langs in this repository are the basic abstract datastructure and algorithms
+Including LinkedList, binarytree, binary-search-tree, stack, queue,hash-table and Graph. All the algorithms are correct which can be applied in these datastructures.
 
-You can git these code and run them, every file is avaliable.If you have some question, please contract with me by the
-email that is cliviazhou@foxmail.com
+You can git these code and run them, every file is avaliable.If you have some question, please contract with me by the email that is cliviazhou@foxmail.com
